2212f14f650d5619d3f1cf8efccfe35ba2b2ac9b
Louis Thurston-Gibson <louisctg@outlook.com>
2020-11-22 19:02:56 +0000

    Make drawing TileMaps into method, draw sea in out of bounds area, and add border boxes
    The cod for drawing the tilemaps has been turned into a method and it now draws water to help differenciate between the arena and the out of bounds area. A simple square bound have been created using squares that are resized at the edges of the arena.

--- Assets/Scripts/Arena_Manager.cs
+++ Assets/Scripts/Arena_Manager.cs
@@ -17,7 +17,6 @@ public class Arena_Manager : MonoBehaviour
     private TileBase grassRightTile;
     [SerializeField]
     private TileBase grassLeftTile;
-
     [SerializeField]
     private TileBase grassBottomLeftTile;
     [SerializeField]
@@ -32,8 +31,24 @@ public class Arena_Manager : MonoBehaviour
     private Tilemap seaTiles;
 
     [SerializeField]
+    private TileBase seaTile;
+
+    [SerializeField]
+    [Range(2,200)]
     private int tileMapSize = 20;
     private int currentTileMapSize;
+
+    [SerializeField]
+    private GameObject collisionObjects;
+
+    [SerializeField]
+    private GameObject borderPrefab;
+
+    private GameObject topBorder;
+    private GameObject bottomBorder;
+    private GameObject leftBorder;
+    private GameObject rightBorder;
+
 
     // Start is called before the first frame update
     void Start()
@@ -41,34 +56,58 @@ public class Arena_Manager : MonoBehaviour
         groundTiles = transform.Find("Ground_Tiles").GetComponent<Tilemap>();
         groundBorderTiles = transform.Find("Ground_Border_Tiles").GetComponent<Tilemap>();
         seaTiles = transform.Find("Sea_Tiles").GetComponent<Tilemap>();
+        DrawTilemaps();
+    }
 
-        //NOTE: Convert into method
+    private void DrawTilemaps()
+    {
+        currentTileMapSize = tileMapSize;
+
+        // Clear all tileMaps
         groundTiles.ClearAllTiles();
-        for(int i = 0; i < tileMapSize; i++)
+        seaTiles.ClearAllTiles();
+
+        // Draw the ground
+        for (int i = 0; i < currentTileMapSize; i++)
         {
-            for(int j = 0; j < tileMapSize; j++)
+            for (int j = 0; j < currentTileMapSize; j++)
             {
                 groundTiles.SetTile(new Vector3Int(i, j, 0), grassTile);
             }
         }
 
-        currentTileMapSize = tileMapSize;
+        // Draw the sea (i.e. to differenciate between the ground and out of bounds area)
+        for (int i = -5; i < currentTileMapSize + 5; i++)
+        {
+            for (int j = -5; j < currentTileMapSize + 5; j++)
+            {
+                seaTiles.SetTile(new Vector3Int(i, j, 0), seaTile);
+            }
+        }
+
+        collisionObjects.transform.position = new Vector2(currentTileMapSize / 2.0f, currentTileMapSize / 2.0f);
+
+        // Instansiate border
+        borderPrefab.transform.localScale = new Vector3(1, currentTileMapSize);
+
+        Destroy(topBorder);
+        Destroy(bottomBorder);
+        Destroy(leftBorder);
+        Destroy(rightBorder);
+
+        topBorder = Instantiate(borderPrefab, new Vector2(currentTileMapSize / 2.0f, currentTileMapSize + 0.5f), Quaternion.Euler(0, 0, 90), collisionObjects.transform);
+        bottomBorder = Instantiate(borderPrefab, new Vector2(currentTileMapSize / 2.0f, -0.5f), Quaternion.Euler(0, 0, 90), collisionObjects.transform);
+
+        leftBorder = Instantiate(borderPrefab, new Vector2(-0.5f, currentTileMapSize / 2.0f), Quaternion.identity, collisionObjects.transform);
+        rightBorder = Instantiate(borderPrefab, new Vector2(currentTileMapSize + 0.5f, currentTileMapSize / 2.0f), Quaternion.identity, collisionObjects.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentTileMapSize != tileMapSize)
+        if (currentTileMapSize != tileMapSize)
         {
-            groundTiles.ClearAllTiles();
-            for (int i = 0; i < tileMapSize; i++)
-            {
-                for (int j = 0; j < tileMapSize; j++)
-                {
-                    groundTiles.SetTile(new Vector3Int(i, j, 0), grassTile);
-                }
-            }
+            DrawTilemaps();
         }
-        currentTileMapSize = tileMapSize;
     }
 }
